fd11add4562590b72d663148ebd2870685f720d3
Avinash Singh <104001259+avinashsng4161@users.noreply.github.com>
2023-07-04 21:31:16 +0530

    Part-8 React JSX

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import User from "./User.jsx";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <h1>Hello World !!</h1>
+
+        <User />
+      </div>
+    </>
+  );
+}
+
+export default App;
