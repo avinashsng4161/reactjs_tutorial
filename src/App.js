fca127e9b43dc4c513677dd1ba82e16da0aca740
Avinash Singh <104001259+avinashsng4161@users.noreply.github.com>
2023-07-06 22:44:24 +0530

    Par-15 Show, Hide & Toggle Tutorial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, {useState} from "react";
+import './App.css';
 
 function App() {
+  const [status, setStatus] = useState(true);
   return (
     <div className="App">
-      <h1>Hello World !!</h1>
+      <h1>Show, Hide & Toggle</h1>
+      {status ? <div className="status">Content</div> : null }
+      
+      {/* Example 1 */}
+      <button onClick={() => setStatus(false)}>Hide</button>
+      <button onClick={() => setStatus(true)}>Show</button>
+
+      {/* Example 2 Toggle */}
+      <button onClick={() => setStatus(!status)}>Toggle</button>
     </div>
   );
 }
+
 export default App;
