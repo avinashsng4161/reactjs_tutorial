3f4254741a187995cf1b4abbf579d4007cc60bce
Avinash Singh <104001259+avinashsng4161@users.noreply.github.com>
2023-07-06 22:44:37 +0530

    Part-16 Form Handling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import FormHandling from './FormHandling';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World !!</h1>
+      <h1>Form Handling</h1>
+      <FormHandling/>
     </div>
   );
 }
+
 export default App;
